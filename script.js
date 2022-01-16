fd26f63297c47c6d50b08c144aa3a2cba32e7d1d
Ernie <anumahjoshuaeneye@gmail.com>
2022-01-16 03:23:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// let btn = document.querySelector(".btn");
+// let x = 1;
+
+// btn.style.padding = "10px 20px";
+// // for(x=1; x < 5; x++){
+// btn.addEventListener("click", function(){
+//     // console.log("hello dear")
+//     if(x> 10){
+//         console.log("Limitation passed");
+//     }else{
+//         console.log(x);
+//     }
+//     x++;
+// })
+
+// let change = document.querySelector("#change")
+// let toggle = document.querySelector(".toggle")
+
+// change.addEventListener("change", function(){
+//     let y = addEventListener("toggle",function(){
+//         toggle.innerHTML = "Changed!"
+//     })
+// })
+
+// change.removeEventListener("change", function(){
+//     toggle.innerHTML = ""
+// })
